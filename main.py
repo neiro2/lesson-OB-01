0231709113e3341db2768082247e676f9a5356d6
neiro2 <neirouniversiti.@gmail.com>
2024-12-21 13:32:57 +0300

    add redaction komment

--- main.py
+++ main.py
@@ -8,12 +8,10 @@ class Task:
         self.deadline = deadline        # Срок выполнения
         self.completed = False          # Статус выполнения: по умолчанию задача не выполнена
 
-    def mark_as_completed(self):
-        """Отметить задачу как выполненную."""
+    def mark_as_completed(self): #Отметить задачу как выполненную.
         self.completed = True
 
-    def __str__(self):
-        """Возвращает строковое представление задачи."""
+    def __str__(self): #Возвращает строковое представление задачи.
         status = "Выполнено" if self.completed else "Не выполнено"
         return f"Описание: {self.description}, Срок: {self.deadline}, Статус: {status}"
 
@@ -26,15 +24,13 @@ class TaskManager:
         task = Task(description, deadline)
         self.tasks.append(task)
 
-    def mark_task_completed(self, index):
-        """Отметить задачу по заданному индексу как выполненную."""
+    def mark_task_completed(self, index): #Отметить задачу по заданному индексу как выполненную.
         if 0 <= index < len(self.tasks):
             self.tasks[index].mark_as_completed()
         else:
             print("Некорректный индекс задачи.")
 
-    def get_pending_tasks(self):
-        """Получить список невыполненных задач."""
+    def get_pending_tasks(self): #Получить список невыполненных задач.
         return [task for task in self.tasks if not task.completed]
 
     def show_tasks(self):
